069c02ef9f392b7b8c39b900aab602f7a1e8bb7f
Mitesh Vaghela <miteshvaghela1999@gmail.com>
2025-01-18 12:51:37 +0530

    Working on Disputes

--- src/components/Layout/GuestLayout/index.tsx
+++ src/components/Layout/GuestLayout/index.tsx
@@ -2,7 +2,7 @@ import { Box } from "@mui/material";
 import React from "react";
 import Header from "../../comman/Header";
 import Footer from "../../comman/Footer";
-import BackImage from "../../../assets/images/background.svg";
+// import BackImage from "../../../assets/images/background.svg";
 import { useLocation } from "react-router-dom";
 interface GuestLayoutProps {
   children: React.ReactNode;
@@ -20,7 +20,7 @@ const GuestLayout: React.FC<GuestLayoutProps> = ({ children }) => {
           transform: "translateX(-50%)", // Center it horizontally
           width: "100%", // Central area width
           height: "600px", // Full height of the container
-          backgroundImage: `url(${BackImage})`, // Use the imported image
+          // backgroundImage: `url(${BackImage})`, // Use the imported image
           backgroundSize: "cover",
           backgroundPosition: "center",
           zIndex: -1,
