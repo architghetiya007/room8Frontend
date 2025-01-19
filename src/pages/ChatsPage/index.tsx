e8c16d3550aea1021fbf2b31ca35cc71b026afda
Mitesh Vaghela <miteshvaghela1999@gmail.com>
2025-01-19 09:06:24 +0530

    TSR-270 Fix

--- src/pages/ChatsPage/index.tsx
+++ src/pages/ChatsPage/index.tsx
@@ -9,6 +9,7 @@ import {
   OutlinedInput,
   Stack,
   Typography,
+  useMediaQuery,
 } from "@mui/material";
 import { formatDistanceToNow } from "date-fns"; // Import date-fns for time formatting
 import {
@@ -23,19 +24,30 @@ import { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { db } from "../../firebase";
 import { RootState } from "../../store";
-import { Navigate, Outlet, useNavigate, useParams } from "react-router-dom";
+import {
+  Navigate,
+  Outlet,
+  useLocation,
+  useNavigate,
+  useParams,
+} from "react-router-dom";
 import { Search } from "@mui/icons-material";
 
 const ChatsPage: React.FC = () => {
+  const isMobile = useMediaQuery("(max-width:600px)");
+  const location = useLocation();
   const { chatId } = useParams();
   const userSlice = useSelector((state: RootState) => state.user);
   const [chatUsers, setChatUsers] = useState<any[]>([]);
   const [searchText, setSearchText] = useState<string>("");
   const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
+  const isChatPage = location.pathname !== "/messages";
+
   if (!userSlice.user) {
     return <Navigate to="/" replace />;
   }
+
   useEffect(() => {
     const fetchChatUsers = () => {
       const chatQuery = query(
@@ -124,195 +136,203 @@ const ChatsPage: React.FC = () => {
   });
 
   return (
-    <Container sx={{
-      px: {
-        xs: 2,
-        md: "0 !important"
-      }
-    }}>
-      <Card
-        sx={{
-          width: "100%", // Full width for smaller screens
-          boxShadow: "0 4px 20px rgba(0, 0, 0, 0.1)",
-          borderRadius: "12px",
-          height: "calc(100vh - 115px)",
-          mt: "-25px",
-        }}
-      >
-        <CardContent
+    <Container
+      sx={{
+        px: {
+          xs: 2,
+          md: "0 !important",
+        },
+      }}
+    >
+      {!isMobile || !isChatPage ? (
+        <Card
           sx={{
-            px: {
-              xs: 1,
-              md: 2,
-            },
-            height: "calc(100vh - 450px)",
+            width: "100%", // Full width for smaller screens
+            boxShadow: "0 4px 20px rgba(0, 0, 0, 0.1)",
+            borderRadius: "12px",
+            height: "calc(100vh - 115px)",
+            mt: "-25px",
           }}
         >
-          <Typography
+          <CardContent
             sx={{
-              fontSize: "50px",
-              background:
-                "linear-gradient(to right, #4AB1F1 0%, #566CEC 33%, #D749AF 66%, #FF7C51 100%)",
-              backgroundClip: "text",
-              WebkitTextFillColor: "transparent",
-              fontWeight: "700",
+              px: {
+                xs: 1,
+                md: 2,
+              },
+              height: "calc(100vh - 450px)",
             }}
-            mb={2}
           >
-            Chats
-          </Typography>
-          {isLoading && (
-            <Box
+            <Typography
               sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                height: "calc(100vh - 400px)",
-                flexDirection: "column",
-                gap: 2,
+                fontSize: "50px",
+                background:
+                  "linear-gradient(to right, #4AB1F1 0%, #566CEC 33%, #D749AF 66%, #FF7C51 100%)",
+                backgroundClip: "text",
+                WebkitTextFillColor: "transparent",
+                fontWeight: "700",
               }}
+              mb={2}
             >
-              <CircularProgress />
-              <Typography variant="h5">Loading Chats....</Typography>
-            </Box>
-          )}
-          {!isLoading && chatUsers.length == 0 && (
-            <Box
-              sx={{
-                display: "flex",
-                alignItems: "center",
-                justifyContent: "center",
-                height: "calc(100vh - 400px)",
-                flexDirection: "column",
-                gap: 2,
-              }}
-            >
-              <Typography>You don't have any conversations yet...</Typography>
-            </Box>
-          )}
-          {!isLoading && chatUsers.length > 0 && (
-            <Grid container spacing={2}>
-              <Grid item xs={12} md={6}>
-                <Grid container spacing={2}>
-                  {chatUsers.length > 0 && (
-                    <Grid item xs={12} px={2} py={0}>
-                      <OutlinedInput
-                        value={searchText}
-                        onChange={(e) => setSearchText(e.target.value)}
-                        fullWidth
-                        size="small"
-                        placeholder="Search By Name"
-                        endAdornment={
-                          <Box
-                            sx={{
-                              backgroundColor: "black",
-                              height: "65px",
-                              display: "flex",
-                              alignItems: "center",
-                              borderTopRightRadius: "8px",
-                              borderBottomRightRadius: "8px",
-                              width: "60px",
-                              justifyContent: "center",
-                              marginRight: "-15px",
-                            }}
-                          >
-                            <Search sx={{ color: "white" }} />
-                          </Box>
-                        }
-                      />
-                    </Grid>
-                  )}
-
-                  {filterChats.map((item) => {
-                    return (
-                      <Grid key={item} item xs={12} px={2} py={1}>
-                        <Stack
-                          onClick={() =>
-                            navigate(`${item.threadId}/${item.id}`)
-                          }
-                          direction={"row"}
-                          alignItems={"center"}
-                          spacing={3}
-                          sx={{
-                            backgroundColor:
-                              chatId === item.threadId ? "#F4F6FA" : "none",
-                            borderRadius: "12px",
-                            px: 2,
-                            py: 2,
-                            borderLeft:
-                              chatId === item.threadId
-                                ? "4px solid #FF445E"
-                                : "none",
-                            cursor: "pointer",
-                            "&:hover": {
-                              backgroundColor: "#F4F6FA",
-                              borderLeft: "4px solid #FF445E",
-                            },
-                          }}
-                        >
-                          {item.profilePic ? (
-                            <Avatar src={item.profilePic}></Avatar>
-                          ) : (
-                            <Avatar>{item.fullName.charAt(0)}</Avatar>
-                          )}
-
-                          <Stack direction={"column"} flex={1}>
-                            <Typography
-                              variant="h6"
-                              sx={{ color: "#3B3D44", fontWeight: "600" }}
-                            >
-                              {item.fullName}
-                            </Typography>
-                            <Typography
-                              variant="subtitle1"
-                              sx={{ color: "#3B3D44", fontWeight: "500" }}
-                            >
-                              {item.threadData.lastMessage?.text}
-                            </Typography>
-                          </Stack>
-                          <Stack direction={"column"} alignItems={"flex-end"}>
-                            <Typography
-                              variant="subtitle1"
-                              sx={{ color: "#929BAB", fontWeight: "450" }}
-                            >
-                              {item.threadData.lastMessage &&
-                              item.threadData.lastMessage.timestamp?.seconds
-                                ? formatDistanceToNow(
-                                    new Date(
-                                      item.threadData.lastMessage.timestamp
-                                        ?.seconds * 1000
-                                    )
-                                  ) + " ago"
-                                : ""}
-                            </Typography>
-                            <Avatar
+              Chats
+            </Typography>
+            {isLoading && (
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: "calc(100vh - 400px)",
+                  flexDirection: "column",
+                  gap: 2,
+                }}
+              >
+                <CircularProgress />
+                <Typography variant="h5">Loading Chats....</Typography>
+              </Box>
+            )}
+            {!isLoading && chatUsers.length == 0 && (
+              <Box
+                sx={{
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  height: "calc(100vh - 400px)",
+                  flexDirection: "column",
+                  gap: 2,
+                }}
+              >
+                <Typography>You don't have any conversations yet...</Typography>
+              </Box>
+            )}
+            {!isLoading && chatUsers.length > 0 && (
+              <Grid container spacing={2}>
+                <Grid item xs={12} md={6}>
+                  <Grid container spacing={2}>
+                    {chatUsers.length > 0 && (
+                      <Grid item xs={12} px={2} py={0}>
+                        <OutlinedInput
+                          value={searchText}
+                          onChange={(e) => setSearchText(e.target.value)}
+                          fullWidth
+                          size="small"
+                          placeholder="Search By Name"
+                          endAdornment={
+                            <Box
                               sx={{
-                                width: "20px",
-                                height: "20px",
-                                bgcolor: "red",
-                                color: "white",
-                                fontSize: "10px",
-                                mt: 1,
+                                backgroundColor: "black",
+                                height: "65px",
+                                display: "flex",
+                                alignItems: "center",
+                                borderTopRightRadius: "8px",
+                                borderBottomRightRadius: "8px",
+                                width: "60px",
+                                justifyContent: "center",
+                                marginRight: "-15px",
                               }}
                             >
-                              {item.threadData.unreadCount[
-                                userSlice.user?._id?.toString() ?? ""
-                              ] || 0}
-                            </Avatar>
-                          </Stack>
-                        </Stack>
+                              <Search sx={{ color: "white" }} />
+                            </Box>
+                          }
+                        />
                       </Grid>
-                    );
-                  })}
+                    )}
+
+                    {filterChats.map((item) => {
+                      return (
+                        <Grid key={item} item xs={12} px={2} py={1}>
+                          <Stack
+                            onClick={() =>
+                              navigate(`${item.threadId}/${item.id}`)
+                            }
+                            direction={"row"}
+                            alignItems={"center"}
+                            spacing={3}
+                            sx={{
+                              backgroundColor:
+                                chatId === item.threadId ? "#F4F6FA" : "none",
+                              borderRadius: "12px",
+                              px: 2,
+                              py: 2,
+                              borderLeft:
+                                chatId === item.threadId
+                                  ? "4px solid #FF445E"
+                                  : "none",
+                              cursor: "pointer",
+                              "&:hover": {
+                                backgroundColor: "#F4F6FA",
+                                borderLeft: "4px solid #FF445E",
+                              },
+                            }}
+                          >
+                            {item.profilePic ? (
+                              <Avatar src={item.profilePic}></Avatar>
+                            ) : (
+                              <Avatar>{item.fullName.charAt(0)}</Avatar>
+                            )}
+
+                            <Stack direction={"column"} flex={1}>
+                              <Typography
+                                variant="h6"
+                                sx={{ color: "#3B3D44", fontWeight: "600" }}
+                              >
+                                {item.fullName}
+                              </Typography>
+                              <Typography
+                                variant="subtitle1"
+                                sx={{ color: "#3B3D44", fontWeight: "500" }}
+                              >
+                                {item.threadData.lastMessage?.text}
+                              </Typography>
+                            </Stack>
+                            <Stack direction={"column"} alignItems={"flex-end"}>
+                              <Typography
+                                variant="subtitle1"
+                                sx={{ color: "#929BAB", fontWeight: "450" }}
+                              >
+                                {item.threadData.lastMessage &&
+                                item.threadData.lastMessage.timestamp?.seconds
+                                  ? formatDistanceToNow(
+                                      new Date(
+                                        item.threadData.lastMessage.timestamp
+                                          ?.seconds * 1000
+                                      )
+                                    ) + " ago"
+                                  : ""}
+                              </Typography>
+                              <Avatar
+                                sx={{
+                                  width: "20px",
+                                  height: "20px",
+                                  bgcolor: "red",
+                                  color: "white",
+                                  fontSize: "10px",
+                                  mt: 1,
+                                }}
+                              >
+                                {item.threadData.unreadCount[
+                                  userSlice.user?._id?.toString() ?? ""
+                                ] || 0}
+                              </Avatar>
+                            </Stack>
+                          </Stack>
+                        </Grid>
+                      );
+                    })}
+                  </Grid>
                 </Grid>
+                {!isMobile && (
+                  <Grid item xs={12} md={6}>
+                    <Outlet />
+                  </Grid>
+                )}
               </Grid>
-              <Grid item xs={12} md={6}>
-                <Outlet />
-              </Grid>
-            </Grid>
-          )}
-        </CardContent>
-      </Card>
+            )}
+          </CardContent>
+        </Card>
+      ) : (
+        isMobile && <Outlet />
+      )}
     </Container>
   );
 };
